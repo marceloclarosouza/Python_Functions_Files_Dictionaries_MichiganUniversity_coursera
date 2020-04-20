5ca01310c9ed6564f74bc6af0edf8dfbe861daba
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-04-20 09:29:57 -0300

    final

---
+++ week2/ex2_week2.py
@@ -0,0 +1,7 @@
+#You are keeping track of olympic medals for Italy in the 2016 Rio Summer Olympics! At the moment, Italy has 7 gold medals, 8 silver metals, and 6 bronze medals. Create a dictionary called olympics where the keys are the types of medals, and the values are the number of that type of medals that Italy has won so far.
+olympics = {}
+
+olympics["gold"] = 7
+olympics["silver"] = 8
+olympics["bronze"] = 6
+print(olympics)
