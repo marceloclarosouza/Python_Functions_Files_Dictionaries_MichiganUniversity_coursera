80c304682b41fc8001c1606e956b73eea71c05a5
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-04-20 09:26:28 -0300

    final

---
+++ week2/ex1_week2.py
@@ -0,0 +1,6 @@
+#Create a dictionary that keeps track of the USA’s Olympic medal count. Each key of the dictionary should be the type of medal (gold, silver, or bronze) and each key’s value should be the number of that type of medal the USA’s won. Currently, the USA has 33 gold medals, 17 silver, and 12 bronze. Create a dictionary saved in the variable medals that reflects this information.
+medals = {}
+medals["gold"] = 33
+medals["silver"] = 17
+medals["bronze"] = 12
+print(medals)
