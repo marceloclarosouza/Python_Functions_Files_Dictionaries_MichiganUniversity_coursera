089a355c89275c2df19237d1a3ceb50da9228f1e
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-04-28 16:28:44 -0300

    week3

---
+++ week3/ex6_week3.py
@@ -0,0 +1,6 @@
+"""Define a function called information that takes as input, the variables name, birth_year, fav_color, and hometown. It should return a tuple of these variables in this order. """
+
+def information(name, birth_year, fav_color, hometown):
+    return name, birth_year, fav_color, hometown
+
+print(information("Getrudez", 1986, "pink", "Luffylandia"))
