d68da1ab576351fd806b69986a2899746d130222
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-04-28 16:08:52 -0300

    week3

---
+++ week3/ex4_week3.py
@@ -0,0 +1,9 @@
+#Provided is a list of tuples. Create another list called t_check that contains the third element of every tuple
+
+lst_tups = [('Articuno', 'Moltres', 'Zaptos'), ('Beedrill', 'Metapod', 'Charizard', 'Venasaur', 'Squirtle'), ('Oddish', 'Poliwag', 'Diglett', 'Bellsprout'), ('Ponyta', "Farfetch'd", "Tauros", 'Dragonite'), ('Hoothoot', 'Chikorita', 'Lanturn', 'Flaaffy', 'Unown', 'Teddiursa', 'Phanpy'), ('Loudred', 'Volbeat', 'Wailord', 'Seviper', 'Sealeo')]
+t_check = []
+
+for num in lst_tups:
+    t_check.append(num[2])
+    
+print(t_check)
