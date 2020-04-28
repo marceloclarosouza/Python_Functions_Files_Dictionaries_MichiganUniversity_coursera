6ebf50b7c8c57eb4c61f803e261b6ce6347a25e8
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-04-28 16:32:40 -0300

    week3

---
+++ week3/ex7_week3.py
@@ -0,0 +1,7 @@
+"""Define a function called info with the following required parameters: name, age, birth_year, year_in_college, and hometown. The function should return a tuple that contains all the inputted information"""
+
+def info(name, age, birth_year, year_in_college, hometown):
+    return name, age, birth_year, year_in_college, hometown
+
+print(info("Gertrudez", 35, 1978, 11, "Zumbilandia"))
+    
