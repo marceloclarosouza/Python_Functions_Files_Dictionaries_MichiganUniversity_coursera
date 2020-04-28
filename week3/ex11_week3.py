714474154984695b0f094c8e7484eef952328b03
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-04-28 17:32:20 -0300

    week3

---
+++ week3/ex11_week3.py
@@ -0,0 +1,3 @@
+"""With only one line of code, assign the variables water, fire, electric, and grass to the values “Squirtle”, “Charmander”, “Pikachu”, and “Bulbasaur” """
+
+water, fire, electric, grass = 'Squirtle', 'Charmander', 'Pikachu', 'Bulbasaur' 
