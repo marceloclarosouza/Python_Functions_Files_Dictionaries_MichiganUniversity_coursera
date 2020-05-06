d6592eb5442e975099589c4384d492d3235d7172
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-05-06 09:31:15 -0300

    Create exe5.py

---
+++ week4/exe5.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Write a function called str_mult that takes in a required 
+string parameter and an optional integer parameter.
+The default value for the integer parameter should be 3.
+The function should return the string multiplied by the integer parameter.
+"""
+def str_mult(x, y = 1):
+    return x*y
+
+print(str_mult(1))
+
